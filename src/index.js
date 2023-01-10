3a44b4a31d49d48c2d5748d5f4fb5ddf678f5c9d
Matheus Ghignatti <matheusghignatti@gmail.com>
2023-01-10 00:47:35 -0300

    add Navbar UI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
+const theme = createTheme({});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   // eslint-disable-next-line comma-dangle
   document.getElementById('root')
 );
