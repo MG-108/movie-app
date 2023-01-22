188b977334aa2aae5293586d3690ab240a7dc749
Matheus Ghignatti <matheusghignatti@gmail.com>
2023-01-22 00:44:51 -0300

    Alan AI part 1

---
+++ src/components/Alan.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useContext } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils';
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+
+  useEffect(() => {
+    alanBtn({
+      key: 'df3c97a9d3718c9b770cd3ce7d31f27f2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, mode }) => {
+        if (command === 'changeMode') {
+          if (mode === 'light') {
+            setMode('light');
+          } else {
+            setMode('dark');
+          }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.clear();
+
+          window.location.href = '/';
+        }
+      },
+    });
+  }, []);
+};
+
+export default useAlan;
